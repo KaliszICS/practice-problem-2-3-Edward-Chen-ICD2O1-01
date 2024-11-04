e3476b6728093d94946f1ec5ba6521519be8496a
Edward-Chen-ICD2O1-01 <edward.chen2@student.tdsb.on.ca>
2024-11-04 17:16:43 -0500

    Update PP2_3.py

--- PP2_3.py
+++ PP2_3.py
@@ -2,27 +2,36 @@
 
 def q1(): 
   #Write Assignment code here
- word=input("In: ")
- if word.endswith("y"):
-  print("-ies")
- elif word.endswith("ey"):
-  print("-eys")
- elif word.endswith("ife"):
-  print("-ives")
- else:
-  print("-s")
-
-  
-
+  word = input("In: ")
+  if word.endswith("ife"):
+    print("-ives")
+  elif word.endswith("ey"):
+    print("-eys")
+  elif word.endswith("y"):
+    print("-ies")
+  else:
+    print("-s")
 
 def q2(): 
   #Write Assignment code here
-  print()
+  num=int(input("In: "))
+  if num > 0:
+    print(f"{num} is positive")
+  else:
+    print(f"{num} is negative")
 
+def q3():
+  #Write Assignment code here
+  num=float(input("Input a number: "))
+  num1=float(input("Input a number: "))
+  num2=float(input("Input a number: "))
+  if num == num1 == num2:
+    print("Equal")
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
+# q3()
